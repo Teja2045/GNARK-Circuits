eb7d74b457671373a6092ffb6a9789c3e00cc3d2
saiteja <tejargukt123@gmail.com>
2024-05-03 14:30:20 +0530

    chore: comments

--- utils/dummyData.go
+++ utils/dummyData.go
@@ -2,15 +2,12 @@ package utils
 
 import "math/big"
 
-/*
-The BN254 Mimc HashString of bigInt(10) is: 2f81d93f7e87768fcb6ae2ad6c782cf95a0d9dfee24bed956a85f7726f57b839
-*/
-
 type HashedData struct {
 	Data       big.Int
 	HashString string
 }
 
+// The BN254 Mimc HashString of bigInt(10) is: 2f81d93f7e87768fcb6ae2ad6c782cf95a0d9dfee24bed956a85f7726f57b839
 func GetDummyHashedData() HashedData {
 
 	// or we can use bytes durectly also

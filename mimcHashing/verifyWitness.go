c9acb4093bf5f9029dd63b103499ba0e6e83956d
saiteja <tejargukt123@gmail.com>
2024-05-06 14:51:21 +0530

    feat: added another verification method

--- mimcHashing/verifyWitness.go
+++ mimcHashing/verifyWitness.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/consensys/gnark/backend/witness"
+	"github.com/consensys/gnark/frontend"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,6 +21,21 @@ func (assignment *MimcCircuit) VerifyWitness(field *big.Int, publicWitness witne
 
 	return assert.Equal(&assert.CollectT{}, publicWitness, newWitness)
 	//return publicWitness == newWitness
+}
+
+func VerifyWitness2(field *big.Int, publicWitness witness.Witness, publicHash any) bool {
+
+	assignment := MimcCircuit{
+		PreImage: 123,
+		Hash:     publicHash,
+	}
+
+	witness, _ := frontend.NewWitness(&assignment, field)
+	newPublicWitness, _ := witness.Public()
+	fmt.Println("public witness:", publicWitness)
+	fmt.Println("constructed witness:", newPublicWitness)
+
+	return assert.Equal(&assert.CollectT{}, publicWitness, newPublicWitness)
 }
 
 // constructs new public witness using assignment's public inputs
